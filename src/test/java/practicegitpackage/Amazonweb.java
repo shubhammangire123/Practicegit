0d8cdf632362794df26966e4491cd5237e054de6
shubhammangire123 <shubhammangire@gmail.com>
2023-08-02 22:14:25 +0530

    [shubhammangire123] changes in Amazonweb

--- src/test/java/practicegitpackage/Amazonweb.java
+++ src/test/java/practicegitpackage/Amazonweb.java
@@ -11,7 +11,7 @@ public class Amazonweb {
  WebDriver driver = new ChromeDriver();
 	  
 	  driver.get("https://www.amazon.in/");
-	 Thread.sleep(2000);
+	 Thread.sleep(1000);
 	  driver.close();
   }
 }
